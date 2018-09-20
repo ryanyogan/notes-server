79e5c4416906393cc24931233c0d6b9c0174a33d
Ryan Yogan <ryanyogan@gmail.com>
2018-09-20 09:05:11 -0700

    Wired up the basics to get a query going

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-import "dotenv/config";
+import 'dotenv/config';
+
+import cors from 'cors';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+const app = express();
+app.use(cors());
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => ({
+      username: 'Ryan Yogan',
+    }),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Server is listening on port :8000/graphql'); // eslint-disable-line
+});
